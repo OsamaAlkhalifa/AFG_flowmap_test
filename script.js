e7aa1da4a4f68561e8dc0804488fd4ad0d75ea1d
OsamaAlkhalifa <92375149+OsamaAlkhalifa@users.noreply.github.com>
2025-04-20 14:33:15 +0430

    Update script.js

--- script.js
+++ script.js
@@ -270,7 +270,7 @@ require([
       iconNode.classList.toggle('icon-ui-pause');
       iconNode.classList.toggle('icon-ui-red');
       iconNode.classList.toggle('icon-ui-play');
-      iconNode.classList.toggle('icon-ui-green');
+      iconNode.classList.toggle('cus-icon-ui-green');
 
       toggleLayerAnimation(oneToManyLayer);
       //toggleLayerAnimation(manyToOneLayer);
